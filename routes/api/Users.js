aac37b6d98fbbbb3c6fd6283a168482a6f6f2e82
Melinda H <melinda.yw.hardin@gmail.com>
2019-05-17 19:26:44 -0500

    validationResult working

--- routes/api/Users.js
+++ routes/api/Users.js
@@ -1,12 +1,21 @@
 const router = require("express").Router();
+const { check, validationResult } = require('express-validator/check');
 
 
 // @route GET api/users
 // @desc Test route
 // @ access Public (no token required using auth. middleware)
 
-router.post('/', (req, res) =>{
-  console.log (req.body);
+router.post('/',[
+  check('name', 'Name is required.').not().isEmpty(),
+  check ('email', 'Please enter a valid email address').isEmail(),
+  check('password', 'Please enter a password with six or more characters').isLength({ min: 6 })
+],
+ (req, res) =>{
+  const errors = validationResult(req)
+  if (!errors.isEmpty()){
+    return res.status(400).json({ errors: errors.array()});
+  }
   res.send("This is the User Route")
 })
 
