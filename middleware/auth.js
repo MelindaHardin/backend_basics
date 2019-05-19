d25f7686f990a14a04468e7d2d844405ce0a0b1e
Melinda H <melinda.yw.hardin@gmail.com>
2019-05-18 21:27:33 -0500

    middleware

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require ('jsonwebtoken');
+const config = require ('config');
+
+module.exports = function (req, res, next){
+  //get the token from the header key
+  const token = req.header ('x-auth-token');
+
+  //check if there is no token
+  if(!token){
+    return res.status(401).json({ msg: "No token, authorization denied." })
+  }
+  //if there is a token, verify it.
+  try{
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    req.user = decoded.user;
+    next();
+  }catch(err){
+    res.status(401).json({ msg: "Token is not found" });
+  }
+}
